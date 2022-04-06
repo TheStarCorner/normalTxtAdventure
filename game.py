d98c1ffd48733ec25ce8ef01aa7bf903e978e8a5
TheStarCorner <ammonwilson1@gmail.com>
2022-04-06 14:22:05 -0600

    first commit

---
+++ game.py
@@ -0,0 +1,22 @@
+
+def main():
+    print ("Welcome to The Game!")
+    part1()
+
+
+def part1():
+    entered_name = input("Enter your name: ")
+    print(f"Your name is {entered_name}")
+    print()
+    print("Eggs - 1, Cereal - 2")
+    firstChoice = input("It is time for breakfast. What will you eat? (TYPE IN EITHER 1 FOR EGGS OR 2 FOR CEREAL):")
+    if firstChoice == "1":
+        print("You really must like eggs!")
+    elif firstChoice == "2":
+        print(f"Let's hope you have milk, {entered_name}")
+
+    print("That's it for now!")
+
+
+if __name__ == "__main__":
+    main()
